5fa2ffbd032258c3fb5634881c217ab57626ee75
eduardofgomes <eduardofranciscogomes01@gmail.com>
2023-06-14 22:17:53 -0300

    Make some changes on UI of the project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                         className="navbar-img"
                     />
                 </div>
-                <div>{favoritePokemons.length} ❤</div>
+                <div>{favoritePokemons.length} ❤️</div>
             </nav>
         </>
     )
